df9e479bac0233bbc6b9ae05d30aefc93068aae4
Emmanuel Nhan <nhanmanu@gmail.com>
2012-08-07 23:42:51 +0200

    added some ajax to retrieve the user list '

--- static/script/client.js
+++ static/script/client.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	
+	$.get('user', function(data){
+		alert(data);
+		data.forEach(function(value, index, array){
+			var entry = "<tr><td>" + value.name + "</td><td>"+value.email + "</td></tr>";
+			$("#addresses").append(entry);
+		});
+	});
+	
+	
 	$("#submit").click(function(){
 
 		$.post('user', JSON.stringify({name:$("#name").val(), email:$("#email").val() }), function(data){
